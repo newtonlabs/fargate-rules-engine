f356596723df22912add9210b9c397a4df1167e0
Thomas Newton <thomas_newton@mckinsey.com>
2018-07-14 23:48:50 -0400

    change ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = 8000;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 let RuleEngine = require("node-rules");
