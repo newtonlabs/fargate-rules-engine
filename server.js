ff76eb5149239918a9995529dca87e24c8c84e61
Thomas Newton <thomas_newton@mckinsey.com>
2018-07-15 00:25:07 -0400

    better messages

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = 8080;
+const PORT = 9090;
 const HOST = '0.0.0.0';
 
 let RuleEngine = require("node-rules");
